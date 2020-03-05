c6b81fcb5ded292135d76816f2942d1feae74aab
StyxS <mail@styxs.cn>
2020-03-05 19:33:37 +0800

    feature: 引入elementUI，建立后台页面框架

--- src/pages/admin/App.vue
+++ src/pages/admin/App.vue
@@ -1,16 +1,63 @@
 <template>
-  <div id="app">
-  </div>
+    <div id="app" class="container">
+        <el-container class="container">
+            <el-header height="50px"><span>Topilog 后台</span></el-header>
+            <el-container>
+                <el-aside id="sideNav" class="container" width="300px">
+                    <el-menu>
+                        <el-submenu v-for="(menu, mIndex) in Menus" :key="mIndex" :index="mIndex.toString()">
+                            <template slot="title">
+                                <span>{{menu.title}}</span>
+                            </template>
+                            <el-menu-item v-for="(item, iIndex) in menu.items" :key="iIndex" 
+                                :index="mIndex+'-'+iIndex">
+                                {{item.title}}
+                            </el-menu-item>
+                        </el-submenu>
+                    </el-menu>
+                </el-aside>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components:{
-  }
+    name: 'App',
+    components:{
+    },
+    data() {
+        return {
+            Menus: [
+                {
+                    title: '网站',
+                    items: [{
+                        title: '网站设置',
+                        url: 'site/config'
+                    }, {
+                        title: '站点统计',
+                        url: 'site/statistics'
+                    }]
+                }, {
+                    title: '文章',
+                    items: [{
+                        title: '文章管理',
+                        url: 'article/list',
+                    },{
+                        title: '文章统计',
+                        url: 'article/statistics'
+                    }]
+                }
+            ]
+        }
+    }
 }
 </script>
-
 <style>
-
+.container {
+    height: 100%;
+}
 </style>
--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/site.css'
 import App from './App'
 import Axios from 'axios'
+Vue.use(ElementUI);
 Vue.prototype.$axios = Axios
 Vue.prototype.$url = "http://localhost:8080"
 
